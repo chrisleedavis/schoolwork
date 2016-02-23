7e2e1acba8c9c99dfcb3ca178630676d2adf4a1c
chrisleedavis <chrisleedavis@gmail.com>
2016-02-23 18:05:11 -0500

    working on coverage now

--- karma.conf.js
+++ karma.conf.js
@@ -11,7 +11,7 @@ module.exports = (config) => {
             "webpack.config.tests.js": ["webpack"]
         },
         coverageReporter: {
-            type: "text"
+            type: "lcovonly"
         },
         reporters: ["dots", "coverage"],
         singleRun: true,
